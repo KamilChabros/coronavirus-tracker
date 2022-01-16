af5de4ffac8a01dc83689786b36acbd7788ba2b9
KamilChabros <kamilchabros@wp.pl>
2022-01-16 14:27:29 +0100

    Some changes in home.html file;

--- src/main/java/xyz/coronavirustracker/coronavirustracker/controllers/HomeController.java
+++ src/main/java/xyz/coronavirustracker/coronavirustracker/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package xyz.coronavirustracker.coronavirustracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/") // '/' means root url
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("testName", "TEST");
         return "home";
     }
 }
