be1fe94224efd95caa9c29844dc1c15a424b91f2
KamilChabros <kamilchabros@wp.pl>
2022-01-16 14:20:14 +0100

    home.html file created;

--- src/main/java/xyz/coronavirustracker/coronavirustracker/controllers/HomeController.java
+++ src/main/java/xyz/coronavirustracker/coronavirustracker/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package xyz.coronavirustracker.coronavirustracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/") // '/' means root url
     public String home() {
         return "home";
     }
